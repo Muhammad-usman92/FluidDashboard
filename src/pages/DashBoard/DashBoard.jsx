34fc2e08f2844d75af5f2f15cfe610a7649b306a
wabbi-khan <kwahab789@gmail.com>
2022-11-15 16:57:23 +0500

    ok

--- src/pages/DashBoard/DashBoard.jsx
+++ src/pages/DashBoard/DashBoard.jsx
@@ -18,6 +18,7 @@ const DashBoard = () => {
   });
   return (
     <div className='fluid'>
+      {/* Top Section */}
       <div className={css.top}>
         <div className='container'>
           <div className='row'>
@@ -34,67 +35,95 @@ const DashBoard = () => {
           </div>
         </div>
       </div>
+      {/* top 1 */}
       <div className='container mt-4'>
         <div className={css.boxes}>
-          <Box heading='Total Farmers' num='70,000' icon={css.bg1} />
-          <Box heading='Verified Farmers' num='60,000' icon={css.bg2} />
-          <Box heading='Declined Farmers' num='20,000' icon={css.bg3} />
-          <Box heading='Total Districts' num='20' icon={css.bg4} />
-          <Box heading='Total Users' num='1,500' icon={css.bg5} />
-          {/* <div className={css.box}>
-            <div className={css.icon}>
-              {" "}
-              <img src={Farmer} alt='Farmer' />{" "}
-            </div>
-            <div className={css.box_content}>
-              <div className={css.total}>Total Farmers</div>
-              <div className={css.num}>70,000</div>
-            </div>
-          </div>
-          <div className={css.box}>
-            <div className={css.icon}>
-              <img src={Farmer} alt='Farmer' />
-            </div>
-            <div className={css.box_content}>
-              <div className={css.total}>Total Farmers</div>
-              <div className={css.num}>70,000</div>
-            </div>
-          </div>
-          <div className={css.box}>
-            <div className={css.icon}>
-              {" "}
-              <img src={Farmer} alt='Farmer' />{" "}
-            </div>
-            <div className={css.box_content}>
-              <div className={css.total}>Total Farmers</div>
-              <div className={css.num}>70,000</div>
-            </div>
-          </div>
-          <div className={css.box}>
-            <div className={css.icon}>
-              {" "}
-              <img src={Farmer} alt='Farmer' />{" "}
-            </div>
-            <div className={css.box_content}>
-              <div className={css.total}>Total Farmers</div>
-              <div className={css.num}>70,000</div>
-            </div>
-          </div>
-          <div className={css.box}>
-            <div className={css.icon}>
-              {" "}
-              <img src={Farmer} alt='Farmer' />{" "}
-            </div>
-            <div className={css.box_content}>
-              <div className={css.total}>Total Farmers</div>
-              <div className={css.num}>70,000</div>
-            </div>
-          </div> */}
+          <Box
+            heading='Total Farmers'
+            num='70,000'
+            icon={css.bg1}
+            box={css.box}
+          />
+          <Box
+            heading='Verified Farmers'
+            num='60,000'
+            icon={css.bg2}
+            box={css.box}
+          />
+          <Box
+            heading='Declined Farmers'
+            num='20,000'
+            icon={css.bg3}
+            box={css.box}
+          />
+          <Box
+            heading='Total Districts'
+            num='20'
+            icon={css.bg4}
+            box={css.box}
+          />
+          <Box heading='Total Users' num='1,500' icon={css.bg5} box={css.box} />
         </div>
       </div>
+      {/* Middle section */}
       <div className='container mt-4'>
-        <div style={{ width: 600 }}>
-          <BarChart chartData={userData} />
+        <div className={css.pro_users}>
+          <div style={{ width: 600 }}>
+            <BarChart chartData={userData} />
+          </div>
+          <div className={css.online}>
+            <div className={css.online_users}>User Online</div>
+            <div>
+              <ol className={css.users_list}>
+                <div>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                </div>
+                <div>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                  <li>wahab</li>
+                </div>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Bottom sec */}
+      <div className='container my-4'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <div className={css.boxes}>
+              <Box
+                heading='Total Cash'
+                num='700,000'
+                icon={css.bg1}
+                box={css.box2}
+              />
+              <Box
+                heading='Transfered'
+                icon={css.bg2}
+                num='71000,000'
+                box={css.box2}
+              />
+              <Box
+                heading='Remaining'
+                num='70,000'
+                icon={css.bg3}
+                box={css.box2}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
